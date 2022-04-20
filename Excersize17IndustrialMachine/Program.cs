4a1323782b77f4f411190442c338e9494c927400
Simon <simon.pikachu@hotmail.se>
2022-04-20 14:08:35 +0200

    Done enough, minor bugs still exist (like reloading a detail view)

--- Excersize17IndustrialMachine/Program.cs
+++ Excersize17IndustrialMachine/Program.cs
@@ -1,6 +1,9 @@
 using Excersize17IndustrialMachine;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +12,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7071") });
 
 builder.Services.AddSingleton<StateService>();
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
