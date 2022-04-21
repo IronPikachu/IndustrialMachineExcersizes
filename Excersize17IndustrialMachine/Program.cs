c4f255b9833358d9c12741c51d24d035141dcd0c
Simon <simon.pikachu@hotmail.se>
2022-04-21 10:34:55 +0200

    Changed URI

--- Excersize17IndustrialMachine/Program.cs
+++ Excersize17IndustrialMachine/Program.cs
@@ -9,7 +9,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7071") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://excersize18api.azurewebsites.net") });
 
 builder.Services.AddSingleton<StateService>();
 
